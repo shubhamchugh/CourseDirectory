9c828fc8056bef4fc46aa612be9e29364c828063
shubham chugh <chugh.shubham12@gmail.com>
2024-03-16 14:52:33 +0530

    packages migrated

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use App\Models\Course\CourseReview;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Sluggable\SlugOptions;
 
 class User extends Authenticatable
 {
