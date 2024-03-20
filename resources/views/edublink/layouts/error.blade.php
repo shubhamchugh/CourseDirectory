18e8e1a88c92e2ba38808aac953983689405a22b
shubham chugh <chugh.shubham12@gmail.com>
2024-03-20 17:13:57 +0530

    error page setup

---
+++ resources/views/edublink/layouts/error.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('edublink.partials.head')
+
+<body class="sticky-header">
+    <div id="main-wrapper" class="main-wrapper">
+        @include('edublink.partials.header')
+
+        <!--=====================================-->
+        <!--=        404 Area Start            =-->
+        <!--=====================================-->
+        <section class="section-gap-equal error-page-area">
+            <div class="container">
+                <div class="edu-error">
+                    <div class="thumbnail">
+                        <img src="assets/images/others/404.png" alt="404 Error">
+                        <ul class="shape-group">
+                            <li class="shape-1 scene">
+                                <img data-depth="2" src="assets/images/about/shape-25.png" alt="Shape">
+
+                            </li>
+                            <li class="shape-2 scene">
+                                <img data-depth="-2" src="assets/images/about/shape-15.png" alt="Shape">
+                            </li>
+                            <li class="shape-3 scene">
+                                <img data-depth="2" src="assets/images/about/shape-13.png" alt="Shape">
+                            </li>
+                            <li class="shape-4 scene">
+                                <img data-depth="-2" src="assets/images/counterup/shape-02.png" alt="Shape">
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="content">
+                        <h2 class="title">@yield('code') - @yield('message')</h2>
+                        {{-- <h4 class="subtitle">The page you are looking for does not exist.</h4> --}}
+                        <a href="index-one.html" class="edu-btn"><i class="icon-west"></i>Back to Homepage</a>
+                    </div>
+                </div>
+            </div>
+            <ul class="shape-group">
+                <li class="shape-1">
+                    <img src="assets/images/others/map-shape-2.png" alt="Shape">
+                </li>
+            </ul>
+        </section>
+
+        @include('edublink.partials.footer')
+    </div>
+
+    @include('edublink.partials.scripts')
+</body>
+
+</html>
---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,5 @@
+@extends('edublink.layouts.error')
+
+@section('title', __('Unauthorized'))
+@section('code', '401')
+@section('message', __('Unauthorized'))
